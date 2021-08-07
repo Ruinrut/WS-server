791efa8b2b36593d63b3ead603e8c2ea0a436578
Ruinrut <ruinrut@gmail.com>
2021-08-07 17:45:33 +0300

    отправка сообщений всем подключенным клиентам
    -список клиентов
    -каждого клиента слушает своя горутина
    -одна горутина на рассылку новых сообщений

--- server.go
+++ server.go
@@ -17,37 +17,69 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+var channelIO = make(chan []byte)   //канал для передачи сообщения для оповещения
+var channelTypIO = make(chan int)   //канал для передачи типа сообщения
+var allClient = []*websocket.Conn{} //слайс подключенных киентов
 
 //обработчик для шаблона
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	//подключение
 	connect, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Upgrade err:", err)
 		return
 	}
-	defer connect.Close() //закрыть соединение по завершению функции
-	for {
-		//получаем сообщение от клиента
-		msgType, message, err := connect.ReadMessage()
-		if err != nil {
-			log.Println("ReadMessage err:", err)
-			return
+	//добавление нового клиента
+	allClient = append(allClient, connect)
+	go func(connect *websocket.Conn, channelIO chan []byte) {
+		print(connect)
+		fmt.Println(" cient connect")
+		defer connect.Close() //закрыть соединение по завершению функции
+
+		for {
+			//получаем сообщение от клиента
+			msgT, message, err := connect.ReadMessage()
+			if err != nil {
+				log.Println("ReadMessage err:", err)
+				return
+			}
+			//отправляем сообщение на оповещение
+			channelIO <- message
+			//отправляем его тип
+			channelTypIO <- msgT
+			print(connect)
+			fmt.Println(" put msg in ch")
 		}
-		log.Printf("message: %s", message)
-		//отправляем сообщение обратно
-		err = connect.WriteMessage(msgType, message)
-		if err != nil {
-			log.Println("WriteMessage err:", err)
-			return
-		}
-	}
+
+	}(connect, channelIO)
+
 }
 
 func main() {
 	fmt.Println("Run my websocket server")
-	flag.Parse()                     //анализ флагов
+	flag.Parse() //анализ флагов
+
+	//оповещение всех клиентов о новом сообщении
+	go func() {
+		for {
+			select {
+			case msg := <-channelIO:
+				msgT := <-channelTypIO
+				//прохо по всем киентам
+				for cient := range allClient {
+					err := allClient[cient].WriteMessage(msgT, msg)
+					if err != nil {
+						log.Println("WriteMessage err:", err)
+					}
+					print(allClient[cient])
+					fmt.Println(" Resend")
+				}
+			}
+		}
+	}()
+
 	http.HandleFunc("/", wsEndpoint) //связываемся с обработчиком
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
